c206207e8a954e621461eac121276e1d85939cd9
Jose Noriega <jmnoriegav01@iescastelar.com>
2022-11-03 12:47:57 +0100

    Subir Archivos

---
+++ Programacion/T1-Practica/HolaMundo.java
@@ -0,0 +1,16 @@
+/**
+ * HolaMundo.java
+ *
+ *
+ * @author 
+ * @version 1.00 2022/11/3
+ */
+
+
+public class HolaMundo {
+
+    public static void main (String[] args) {
+		System.out.print("Hola Mundo");    	
+	}
+    
+}
